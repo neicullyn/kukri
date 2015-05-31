d508774e5929656be6e7c151d56958d6cb0cd489
neicullyn <neicullyn@gmail.com>
2015-05-30 19:10:56 -0700

    v3 done

--- src/Kukri_v03.cu
+++ src/Kukri_v03.cu
@@ -42,6 +42,7 @@ __global__ void kukri::_half_mm_v03_kernel(const half *d_A, const half *d_B, hal
 
     float val[_N_LINE_Y_V03];
     int yf[_N_LINE_Y_V03];
+    int yo[_N_LINE_Y_V03];
 
     for (int i = 0; i < _N_LINE_Y_V03; i++) {
         val[i] = 0;
@@ -50,7 +51,10 @@ __global__ void kukri::_half_mm_v03_kernel(const half *d_A, const half *d_B, hal
     for (int i = 0; i < _N_LINE_Y_V03; i++) {
         yf[i] = threadIdx.y + i * _STRIP_Y_V03;
     }
-    
+
+    for (int i = 0; i < _N_LINE_Y_V03; i++) {
+        yo[i] = threadIdx.y * _BOX_V03 + i * _STRIP_Y_V03 * _BOX_V03;
+    }
 
     for (int i_iter = 0; i_iter < n_iter; i_iter++) {
         // Loading the block into shared memory
@@ -62,6 +66,7 @@ __global__ void kukri::_half_mm_v03_kernel(const half *d_A, const half *d_B, hal
             for (int y = threadIdx.y; y < m_limit; y += _STRIP_Y_V03) {
                 // Note that buf_A and buf_B are transposed
                 buf_A[IDX2C(x, y, _BOX_V03)] = __half2float(d_A[IDX2C(y + m_offset, x + k_offset, M)]);
+                //buf_A[IDX2C(x, y, _BOX_V03)] = __half2float(1);
             }
         }
 
@@ -69,6 +74,7 @@ __global__ void kukri::_half_mm_v03_kernel(const half *d_A, const half *d_B, hal
             for (int y = threadIdx.y; y < k_limit; y += _STRIP_Y_V03) {
                 // Note that buf_A and buf_B are transposed
                 buf_B[IDX2C(x, y, _BOX_V03)] = __half2float(d_B[IDX2C(y + k_offset, x + n_offset, K)]);
+                //buf_B[IDX2C(x, y, _BOX_V03)] = __half2float(1);
             }
         }
 
@@ -76,12 +82,16 @@ __global__ void kukri::_half_mm_v03_kernel(const half *d_A, const half *d_B, hal
 
         if (x < n_limit) {
             for (int k = 0; k < k_limit; k++) {
-                float b = buf_B[IDX2C(x, k, _BOX_V03)];
+                float b = buf_B[IDX2C(x, k, _BOX_V03)];               
 
                 for (int i = 0; i < _N_LINE_Y_V03; i++) {
-                    int y = yf[i];
+                    int y = yf[i];                    
+
                     if (y < m_limit) {                    
-                        float a = buf_A[IDX2C(k, y, _BOX_V03)];                        
+                        // a =  buf_A[IDX2C(k, y, _BOX_V03)]
+                        //   = buf_A[y * _BOX_V03 + k]
+                        //   = buf_A[yo[i] + k]
+                        float a = buf_A[yo[i] + k];
                         val[i] += a * b;
                     }
                 }
