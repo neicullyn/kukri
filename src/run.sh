d72e3dad910b41faa306b663c7d247b4814f06bf
neicullyn <neicullyn@gmail.com>
2015-05-31 16:28:43 -0700

    add log to the run script

--- src/run.sh
+++ src/run.sh
@@ -2,4 +2,8 @@ export PATH=$PATH:/usr/local/cuda-6.5/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-6.5/lib64
 
 make -f Makefile
-./kukri_test 1000 1000 1000
+
+mkdir -p log
+
+name=$(date +log/%Y%m%d_%H%M%S.txt)
+./kukri_test 1000 1000 1000 | tee $name
