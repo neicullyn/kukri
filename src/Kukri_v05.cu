31ae4b142365c8a064ce6b591e9e83d605f40a27
neicullyn <neicullyn@gmail.com>
2015-06-01 16:12:19 -0700

    fails?

--- src/Kukri_v05.cu
+++ src/Kukri_v05.cu
@@ -73,20 +73,16 @@ __global__ void kukri::_half_mm_v05_kernel(const half *d_A, int ld_A, const half
             if (2 * x < m_limit) {                
                 for (int y = threadIdx.y; y < k_limit; y += _STRID_Y_V05) {
                     unsigned int temp = *((unsigned int *)&d_A[IDX2C(x * 2 + m_offset, y + k_offset, ld_A)]);
-                    half l = (temp & 0xffff0000) >> 16;
-                    half r = (temp & 0x0000ffff);
-                    buf_A[IDX2C(2 * x, y, _BOX_V05 + 1)] = __half2float(l);
-                    buf_A[IDX2C(2 * x + 1, y, _BOX_V05 + 1)] = __half2float(r);
+                    buf_A[IDX2C(2 * x, y, _BOX_V05 + 1)] = __half2float(*((half *)&temp));
+                    buf_A[IDX2C(2 * x + 1, y, _BOX_V05 + 1)] = __half2float(*(((half *)&temp) + 1));
                 }
             }
 
             if (2 * x < k_limit) {
                 for (int y = threadIdx.y; y < n_limit; y += _STRID_Y_V05) {
                     unsigned int temp = *((unsigned int *)&d_B[IDX2C(x * 2 + k_offset, y + n_offset, ld_B)]);
-                    half l = (temp & 0xffff0000) >> 16;
-                    half r = (temp & 0x0000ffff);
-                    buf_B[IDX2C(2 * x, y, _BOX_V05 + 1)] = __half2float(l);
-                    buf_B[IDX2C(2 * x + 1, y, _BOX_V05 + 1)] = __half2float(r);
+                    buf_B[IDX2C(2 * x, y, _BOX_V05 + 1)] = __half2float(*(((half *)&temp) + 1));
+                    buf_B[IDX2C(2 * x + 1, y, _BOX_V05 + 1)] = __half2float(*(((half *)&temp) + 1));
                 }
             }
         }
